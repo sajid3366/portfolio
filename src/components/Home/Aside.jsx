a3aeaebbd3252a1b6b61def30e0d1d2c173d19f0
sajid360 <ahteshamsajid8@gmail.com>
2023-12-12 18:47:54 +0600

    modified few things

--- src/components/Home/Aside.jsx
+++ src/components/Home/Aside.jsx
@@ -9,7 +9,7 @@ const Aside = () => {
             <h1 className="text-xl">Web Developer</h1>
             <img className='w-[300px] h-[300px] mx-auto mt-5 rounded-tl-[20px] rounded-br-[20px] mb-5' src={image} alt="" />
             <div className='flex gap-x-3 justify-center mb-5'>
-                <a href="https://www.linkedin.com/in/ahtesham-sajid-68aa2022a/" target="_blank" rel="noreferrer">
+                <a href="https://www.linkedin.com/in/ahtesham-sajid-68aa2022a/" target="_blank" rel="noreferrer">   
                     <div className=' border-gray-500 border-[1px] rounded-full p-5'>
 
                         <img className='w-[20px]' src={linkedin} alt="" />
--- src/components/Home/Introduce.jsx
+++ src/components/Home/Introduce.jsx
@@ -1,5 +1,5 @@
 import { MdHome } from "react-icons/md";
-
+import Typewriter from 'react-ts-typewriter';
 const Introduce = () => {
     return (
         <div className="mt-[60px] max-w-4xl">
@@ -7,7 +7,8 @@ const Introduce = () => {
                 <h1 className="border-gray-600 border-[1px] px-3 py-2 rounded-full text-sm text-center mb-10 w-[140px] flex  justify-center items-center gap-x-2"><MdHome></MdHome>INTRODUCE</h1>
                 <div className="ml-[50px]">
                     <h2 className="text-7xl">Ahtesham Sajid</h2>
-                    <h3 className="text-3xl text-red-400 my-3">Web Developer</h3>
+
+                    <h3 className="text-3xl text-red-400 my-3"><Typewriter text='Web Developer' /></h3>
                     <p className="text-[#6c757d]">Full Stack Web Developer. I am working with React, Node.js,Express JS, React Native and MongoDB. I also have experience in developing static websites using HTML, CSS and JavaScript(ES6).I can provide clean code and pixel perfect design.</p>
                     <div className="mt-8 flex  gap-x-16">
                         <div className="w-[100px]">
